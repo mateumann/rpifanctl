f80e4e751e82a79c12bd493feb82134aef394e0e
Mateusz Neumann <mateumann@gmail.com>
2022-03-23 06:31:11 +0100

    Archive latest changes

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-// Rust implementation of ideas from this article:
-// https://www.instructables.com/Control-a-Cooling-Fan-on-a-Raspberry-Pi-3/
-use clap::{value_t, App, AppSettings, Arg, SubCommand};
-use rppal::pwm::{Channel, Polarity, Pwm};
 use std::error::Error;
 use std::fs::File;
 use std::io::Read;
 use std::thread;
 use std::time::Duration;
+
+// Rust implementation of ideas from this article:
+// https://www.instructables.com/Control-a-Cooling-Fan-on-a-Raspberry-Pi-3/
+use clap::{value_t, App, AppSettings, Arg, SubCommand};
+use rppal::pwm::{Channel, Polarity, Pwm};
 
 struct Config {
     pwm: u8,
@@ -19,6 +20,18 @@ struct Config {
 impl Config {
     fn temp_filename(&self) -> String {
         return format!("/sys/class/thermal/thermal_zone{}/temp", self.thermal_zone);
+    }
+    fn pwm_channel(&self) -> Channel {
+        match self.pwm {
+            0 => return Channel::Pwm0,
+            1 => return Channel::Pwm1,
+
+            _ => panic!(
+                "Raspberry Pi has only two PWM channels, {} is not a valid identifier. \
+                         Please use either 0 or 1",
+                self.pwm
+            ),
+        }
     }
 }
 
@@ -86,13 +99,13 @@ fn get_configuration() -> Config {
         .split(|c| c == ',' || c == ' ')
         .map(|x| x.parse().unwrap())
         .collect();
-    return Config{
+    return Config {
         pwm,
         thermal_zone,
         poll_frequency,
         temperature_steps,
         fan_speed_steps,
-    }
+    };
 }
 
 fn get_temperature(path: &str) -> f64 {
@@ -118,28 +131,37 @@ fn calculate_duty_cycle(
         return 0.0;
     }
     for i in 0..temperature_steps.len() - 1 {
-        if temperature_steps[i] <= temperature && temperature < temperature_steps[i+1] {
-            return (fan_speed_steps[i+1] - fan_speed_steps[i]) /
-            (temperature_steps[i+1] - temperature_steps[i]) *
-            (temperature - temperature_steps[i]) +
-            fan_speed_steps[i];
+        if temperature_steps[i] <= temperature && temperature < temperature_steps[i + 1] {
+            return (fan_speed_steps[i + 1] - fan_speed_steps[i])
+                / (temperature_steps[i + 1] - temperature_steps[i])
+                * (temperature - temperature_steps[i])
+                + fan_speed_steps[i];
         }
     }
     return *fan_speed_steps.last().unwrap();
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() {
     let config = get_configuration();
-    let pwm = Pwm::with_frequency(Channel::Pwm0, 25.0, 0.0, Polarity::Normal, true)?;
-    println!("Raspberry Pi Fan Controller using PWM{}, polling every {} seconds", config.pwm, config.poll_frequency);
+    let pwm = Pwm::with_frequency(config.pwm_channel(), 25.0, 0.0, Polarity::Normal, true).unwrap();
+    println!(
+        "Raspberry Pi Fan Controller using PWM{}, polling every {} seconds",
+        config.pwm, config.poll_frequency
+    );
     loop {
         let temperature: f64 = get_temperature(config.temp_filename().as_str());
-        let duty_cycle: f64 = calculate_duty_cycle(&config.temperature_steps, &config.fan_speed_steps, temperature);
-        pwm.set_duty_cycle(duty_cycle)?;
-        println!("Current temperature={}, duty_cycle={}", temperature, duty_cycle);
-        thread::sleep(Duration::from_millis((config.poll_frequency * 1000.0) as u64));
+        let duty_cycle: f64 = calculate_duty_cycle(
+            &config.temperature_steps,
+            &config.fan_speed_steps,
+            temperature,
+        );
+        pwm.set_duty_cycle(duty_cycle);
+        println!(
+            "Current temperature={}, duty_cycle={}",
+            temperature, duty_cycle
+        );
+        thread::sleep(Duration::from_millis(
+            (config.poll_frequency * 1000.0) as u64,
+        ));
     }
-
-
-    Ok(())
 }
